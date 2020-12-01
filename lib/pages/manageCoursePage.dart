3cfd7fc55b91b9cd861b61057b27fe44f2e95bc1
max <>
2020-12-01 17:37:34 +0900

    코스생성 + 코스추천 아이디어

--- lib/pages/manageCoursePage.dart
+++ lib/pages/manageCoursePage.dart
@@ -1,12 +1,27 @@
+import 'package:bluespot/pages/mapPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:bluespot/pages/recommend.dart';
+import 'package:bluespot/pages/pickMarkersPage.dart';
+
 
 class ManageCoursePage extends StatefulWidget {
+
+  final String uid;
+  final User loggeduser;
+  ManageCoursePage({Key key, @required this.uid, this.loggeduser}) : super(key: key);
+
   @override
-  _ManageCoursePageState createState() => _ManageCoursePageState();
+  _ManageCoursePageState createState() => _ManageCoursePageState(this.uid,this.loggeduser);
 }
 
 class _ManageCoursePageState extends State<ManageCoursePage> {
+  final String uid;
+  final User loggeduser;
+
+  _ManageCoursePageState(this.uid, this.loggeduser);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +55,9 @@ class _ManageCoursePageState extends State<ManageCoursePage> {
               margin: EdgeInsets.only(left: 16, top: 32, right: 16),
               child: GestureDetector(
                 onTap: () {
-                  Navigator.pushNamed(context, '/clickCourseMake');
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) =>
+                      PickPage(uid: this.uid,loggeduser: this.loggeduser,)));
                 },
                 child: Stack(
                   alignment: Alignment.center,
@@ -233,7 +250,12 @@ class _ManageCoursePageState extends State<ManageCoursePage> {
               margin: EdgeInsets.only(left: 16, top: 31, right: 16),
               child: GestureDetector(
                 onTap: () {
-
+                  // Navigator.of(context).popUntil((route) => route.isFirst);
+                  // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) =>
+                  //     Recommend(uid: this.uid,loggeduser: this.loggeduser,)));
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) =>
+                      Recommend(uid: this.uid,loggeduser: this.loggeduser,)));
                 },
                 child: Stack(
                   alignment: Alignment.center,
